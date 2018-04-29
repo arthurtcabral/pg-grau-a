a599fe665b17e2b2237e3817711e7ef331c660b7
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2018-04-29 12:25:46 -0300

    fix(SceneManager.cpp): corrigir transparência.

--- HelloTextures/HelloTextures - STB/HelloTriangle/SceneManager.cpp
+++ HelloTextures/HelloTextures - STB/HelloTriangle/SceneManager.cpp
@@ -144,9 +144,8 @@ void SceneManager::render()
 	glClearColor(0.5f, 0, 0, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//TODO: Descomentando isso, a tela fica toda vermelha
-	//glEnable(GL_BLEND);
-	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	shader->Use();
 
